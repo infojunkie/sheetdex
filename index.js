b8177e89801df5c2c441e430ee445cfd396eb8ff
infojunkie <karim.ratib@gmail.com>
2018-09-23 00:01:01 -0700

    Support book-indices repo

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import fs from 'fs';
 import jp from 'jsonpath';
+import Papa from 'papaparse';
 
-const volumes = [];
+const books = [];
+
+// Read our indexes.
 fs.readdirSync('./books').forEach(file => {
-  volumes.push(JSON.parse(fs.readFileSync(`./books/${file}`)));
+  books.push(JSON.parse(fs.readFileSync(`./books/${file}`)));
 });
-const query = process.argv[2];
-const results = jp.nodes(volumes, `$..sheets[?(@.title === '${query}')]`).map(sheet => {
+
+// Read book-indices.
+Papa.parse(fs.readFileSync('./book-indices/Index.csv', 'utf8')).data.map(row => {
   return {
-    book: volumes[sheet.path[1]].title,
+    title: row[0],
+    file: row[1]
+  }
+}).forEach(book => {
+  books.push({
+    title: book.title,
+    sheets: Papa.parse(fs.readFileSync(`./book-indices/${book.file}`, 'utf8')).data.map(row => {
+      return {
+        title: row[0],
+        page: row[1]
+      }
+    })
+  });
+})
+
+// Perform query.
+const query = process.argv[2];
+const results = jp.nodes(books, `$..sheets[?(@.title == '${query}')]`).map(sheet => {
+  return {
+    book: books[sheet.path[1]].title,
     page: sheet.value.page
   }
 });
-console.log("Your results are: ", results);
+console.log(`Searching for "${query}":\n`, results);
