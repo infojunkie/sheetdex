3d6445d58919e24698df4abd5b93a96e8c3b9d93
infojunkie <karim.ratib@gmail.com>
2018-09-24 10:38:23 -0700

    Refactor index creation

--- query.js
+++ query.js
@@ -1,18 +1,12 @@
-import fs from 'fs';
 import jp from 'jsonpath';
-import glob from 'glob';
 import { indexBookIndices } from './book-indices';
 import { indexJameyAebersold } from './jamey-aebersold';
 import { indexGuitarTechniques } from './guitar-techniques';
 
 // Build our index.
-let books = [];
-glob.sync('./books/*.json').forEach(file => {
-  books.push(JSON.parse(fs.readFileSync(file)));
-});
-books = books.concat(indexBookIndices());
-books = books.concat(indexJameyAebersold());
-books = books.concat(indexGuitarTechniques());
+const books = indexBookIndices()
+  .concat(indexJameyAebersold())
+  .concat(indexGuitarTechniques());
 
 // Perform query.
 if (!process.argv[2]) { console.error("No query"); process.exit(0); }
@@ -27,6 +21,4 @@ const results = jp.nodes(books, `$..sheets[?(normalizeCompare(@.title, '${query}
     page: sheet.value.page || '(unknown)'
   }
 });
-
-// Print results.
 console.log(`Query: ${query}\n\n`, results);
